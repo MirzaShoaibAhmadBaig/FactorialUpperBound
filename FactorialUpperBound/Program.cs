23a2ee737897218a971584842c75bf6e83b22957
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-20 22:17:16 +0500

    Add project files.

---
+++ FactorialUpperBound/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FactorialUpperBound
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Upper Bound Value");
+            int UpperboundValue = Int32.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= UpperboundValue; i++)
+            {
+                 int fact = 1;
+                for (int j = 1; j <=i; j++)
+                {
+                    
+                       fact = fact * j; 
+
+                }
+                Console.WriteLine("{0},   {1}", i, fact);
+
+            }
+            
+
+
+
+
+
+
+        }
+    }
+}
